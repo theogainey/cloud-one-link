05c635aa884a6ce8532dff8d3d1bc754a08f9e72
Theo Gainey <theogainey@gmail.com>
2021-09-21 16:10:53 -0400

    Update [...auth0].js

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,12 +1,12 @@
 // pages/api/auth/[...auth0].js
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
-const redirctURI = 'https://cloud-one-link-theogainey.vercel.app/api/auth/login'
 
 export default handleAuth({
   async callback(req, res) {
+
     try {
-      await handleCallback(req, res, { redirctURI });
+      await handleCallback(req, res, { redirectUri: 'https://cloud-one-link.vercel.app/dashboard' });
     } catch (error) {
       res.status(error.status || 500).end(error.message);
     }
