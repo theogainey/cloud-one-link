e81356d0705d48adf846252c1d02d7b70a6f4719
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-09-21 18:15:42 -0400

    fix redirects

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -4,9 +4,8 @@ import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
   async callback(req, res) {
-
     try {
-      await handleCallback(req, res, { redirectUri: 'https://cloud-one-link-theogainey.vercel.app' });
+      await handleCallback(req, res, { redirectUri: 'https://cloud-one-link.vercel.app/dashboard' });
     } catch (error) {
       res.status(error.status || 500).end(error.message);
     }
