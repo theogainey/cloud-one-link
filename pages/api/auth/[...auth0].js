9872045cef239e3a42d6580ad72c8116968f135d
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-09-21 15:30:09 -0400

    override redirectURI

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,4 +1,14 @@
 // pages/api/auth/[...auth0].js
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+const redirctURI = '/dashboard'
+
+export default handleAuth({
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, { redirctURI });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
